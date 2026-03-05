bef1ce68f274765dee9b649f7b4c70b8328e4e84
Hoang Hong Huy <huy.hoanghong.work@gmail.com>
2026-03-05 16:54:41 +0700

    add StoreMetrics model and update data processing workflow to use new metrics structure

---
+++ StoreRevenue_Dispatcher/Models/StoreMetrics.cs
@@ -0,0 +1,9 @@
+namespace StoreRevenue_Dispatcher
+{
+    public class StoreMetrics
+    {
+        public double TotalRevenue { get; set; }
+        public double TotalCost { get; set; }
+        public double TotalProfit { get; set; }
+    }
+}
